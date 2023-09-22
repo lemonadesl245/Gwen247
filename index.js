ff87af7f31ca601e5311af68a937df4d316eb5b1
LemonCake <126799951+lemonadesl245@users.noreply.github.com>
2023-09-22 19:40:12 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const Eris = require("eris");
+const Gwen = require("gwen");
 const keep_alive = require('./keep_alive.js')
 
 // Replace TOKEN with your bot account's token
-const bot = new Eris(process.env.token);
+const bot = new Gwen(process.env.token);
 
 bot.on("error", (err) => {
   console.error(err); // or your preferred logger
